11afc430ab1fabc88696377b933fffeabe440e4f
tayyab hussain <tayyabhussain496@gmail.com>
2023-09-22 11:59:10 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import MobileNavbar from "@/components/MobileNavbar";
 
 export default function Home() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
 }
