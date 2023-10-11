f143124fa08589ae121dffccd2bceef56a7f3c9b
tayyab hussain <tayyabhussain496@gmail.com>
2023-10-11 12:48:27 +0500

    padding & margin changed

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -2,8 +2,8 @@ import Card from "./Card";
 
 const Features = () => {
   return (
-    <section className="mt-32">
-      <div className="mx-10 sm:mx-14 md:mx-16 lg:mx-20 border-t border-black pt-24">
+    <section className="mt-28">
+      <div className="mx-10 sm:mx-14 md:mx-16 lg:mx-20 border-y border-black pt-24 pb-6">
         <p className="text-red">FEATURES</p>
         <h2 className="text-5xl font-extrabold mt-3 text-offWhite">
           What I Do
